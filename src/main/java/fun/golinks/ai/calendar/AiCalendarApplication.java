c6921e367c4045bdc4fd56b6db951aa1fb32d7a9
xincao9 <xincao9@gmail.com>
2025-05-03 13:27:32 +0800

    OK

---
+++ src/main/java/fun/golinks/ai/calendar/AiCalendarApplication.java
@@ -0,0 +1,13 @@
+package fun.golinks.ai.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fun/golinks/ai/calendar/AiCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.golinks.ai.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
