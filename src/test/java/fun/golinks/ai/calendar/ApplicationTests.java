5fe36a20dc17cf30d422935f8f6d18fe99e31ec0
fochat666 <fochat666@gmail.com>
2025-05-29 16:50:40 +0800

    OK

--- src/test/java/fun/golinks/ai/calendar/ApplicationTests.java
+++ src/test/java/fun/golinks/ai/calendar/ApplicationTests.java
@@ -1,8 +1,10 @@
 package fun.golinks.ai.calendar;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class ApplicationTests {
 
